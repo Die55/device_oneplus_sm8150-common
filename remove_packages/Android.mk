c59a00319b7199135bf70d3e41b6de470f411358
Die55 <diego_albense@hotmail.com>
2021-09-02 01:45:27 +0530

    fix commit

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,11 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-<<<<<<< HEAD
 LOCAL_OVERRIDES_PACKAGES := NfcNci MusicFX
-=======
 LOCAL_OVERRIDES_PACKAGES := AudioFX
->>>>>>> d23c2e8... sm8150-common: RIP `remove_packages`
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
